d02316f22e10a70534fbf8cdcf0f57536b8ab9ed
Vivekanandan (Vivek) Balasubramanian <vivek.balasubramanian@rutgers.edu>
2014-12-02 11:05:51 -0500

    Adding post_exec function

--- src/radical/ensemblemd/mdkernels/md_task_description.py
+++ src/radical/ensemblemd/mdkernels/md_task_description.py
@@ -163,6 +163,9 @@ class MDTaskDescription(attributes.Attributes) :
           if 'pre_exec' in kernel and kernel['pre_exec'] is not None:
             pre_exec.extend(kernel['pre_exec'])
 
+          if 'post_exec' in kernel and kernel['post_exec'] is not None:
+            pre_exec.extend(kernel['post_exec'])
+
           # Process local input file copy as part of pre-execution
           if self.copy_local_input_data is not None:
               for lid in self.copy_local_input_data:
